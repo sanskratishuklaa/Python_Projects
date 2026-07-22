0f2236b91628334701924758dc8809e8baed1a93
sanskratishuklaa <193257292+sanskratishuklaa@users.noreply.github.com>
2026-07-23 00:04:48 +0530

    add password-manager

--- password_manager/Password_manager.py
+++ password_manager/Password_manager.py
@@ -14,3 +14,45 @@ except:
 
 def generate_password():
     chars=string.ascii_letters+string.digits+"!@#$%&"
+    password="".join(random.choice(chars) for _ in range(8))
+    return password
+
+while True:
+      print("\n---Personal Password Manager---")
+      print("1.Save Password")
+      print("2.View Password")
+      print("3.Generate Password")
+      print("4.Exit")
+      
+      choice=input("Enter your choice: ")
+      
+      if choice=="1":
+          site=input("Enter Website: ")
+          pwd=input("Enter Password: ")
+          
+          passwords[site]=pwd
+          
+          with open("password.txt","a") as file:
+              file.write(f"{site}:{pwd}\n")
+              
+          print("Saved!")
+          
+      elif choice=="2":
+          if not passwords:
+              print("No data")
+          else:
+              for site, pwd in passwords.item():
+                  print(site , ":", pwd)
+
+        
+      elif choice=="3":
+          print("Generated password: ", generate_password())
+      
+      elif choice=="4":
+          print("Thanks!")
+       
+      else:
+          print("In-valid Input")
+          
+    
+    
